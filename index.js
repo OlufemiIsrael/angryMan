0f3c6ecdb3c14180bcf3567cc947d98d5e9e18de
olufemi isarel <olufemiisraelpelumi@gmail.com>
2021-12-21 16:59:53 +0100

    ....

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("div-one").style.display = "none"
+document.getElementById("hamburger").onclick = function name(params) {
+    document.getElementById("div-one").style.display = "block"
+    document.getElementById("hamburger").style.display = "none"
+    
+}
